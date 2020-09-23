81d17dd0344f33e2cf91fc3c43c70c0114709e35
TheLittleCitizen9 <thelittlecitizen9@gmail.com>
2020-09-23 20:24:09 +0300

    Started adding components

---
+++ src/components/input.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function KeyPad() {
+  return (
+    <div className="keyPad">
+      <button name="1">1</button>
+      <button name="2">2</button>
+      <button name="3">3</button>
+      <button name="4">4</button>
+      <button name="5">5</button>
+      <button name="6">6</button>
+      <button name="7">7</button>
+      <button name="8">8</button>
+      <button name="9">9</button>
+      <button name="0">0</button>
+      <button name="+">+</button>
+      <button name="-">-</button>
+      <button name="%">%</button>
+      <button name="*">*</button>
+    </div>
+  );
+}
+
+export default KeyPad;
---
+++ src/components/viewComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ViewWindow(props) {
+  return (
+    <div className="view">
+      <p>{props.result}</p>
+    </div>
+  );
+}
+
+export default ViewWindow;
