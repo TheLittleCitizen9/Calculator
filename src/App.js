c443e8be3ba2a42db5e91421118985b67d31e517
TheLittleCitizen9 <thelittlecitizen9@gmail.com>
2020-09-23 20:41:17 +0300

    Designed the calculator

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <ViewWindow result="Hi"/>
-      <KeyPad></KeyPad>
+      <KeyPad/>
     </div>
   );
 }
--- src/components/input.js
+++ src/components/input.js
@@ -2,21 +2,32 @@ import React from 'react';
 
 function KeyPad() {
   return (
-    <div className="keyPad">
-      <button name="1">1</button>
-      <button name="2">2</button>
-      <button name="3">3</button>
-      <button name="4">4</button>
-      <button name="5">5</button>
-      <button name="6">6</button>
-      <button name="7">7</button>
-      <button name="8">8</button>
-      <button name="9">9</button>
-      <button name="0">0</button>
-      <button name="+">+</button>
-      <button name="-">-</button>
-      <button name="%">%</button>
-      <button name="*">*</button>
+    <div>
+        <div className="row">
+            <button className="col-3" name="1">1</button>
+            <button className="col-3" name="2">2</button>
+            <button className="col-3" name="3">3</button>
+        </div>
+        <div className="row">
+            <button className="col-3" name="4">4</button>
+            <button className="col-3" name="5">5</button>
+            <button className="col-3" name="6">6</button>
+        </div>
+        <div className="row">
+            <button className="col-3" name="7">7</button>
+            <button className="col-3" name="8">8</button>
+            <button className="col-3" name="9">9</button>
+        </div>
+        <div className="row">
+            <button className="col-3" name="0">0</button>
+            <button className="col-3" name="+">+</button>
+            <button className="col-3" name="-">-</button>
+        </div>
+        <div className="row">
+            <button className="col-3" name="%">%</button >
+            <button className="col-3" name="*">*</button>
+            <button className="col-3" name="CE">CE</button>
+        </div>
     </div>
   );
 }
