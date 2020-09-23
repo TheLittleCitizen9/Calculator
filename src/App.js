46fb28bce5b322f4427b072b0ba9f213fabba9e2
TheLittleCitizen9 <thelittlecitizen9@gmail.com>
2020-09-23 21:30:46 +0300

    Have a calculator working

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import ViewWindow from './components/viewComponent'
 import KeyPad from './components/input'
 
 function App() {
+  const [result, setResult] = useState("")
+
+  const buttonPressed = btnName => {
+    if(btnName === "="){
+      calculate()
+    }else if(btnName === "CE"){
+      setResult("")
+    }else{
+      setResult(result + btnName)
+    }
+  }
+
+  const calculate = () => {
+    setResult(eval(result))
+  }
+
   return (
     <div className="App">
-      <ViewWindow result="Hi"/>
-      <KeyPad/>
+      <ViewWindow result={result}/>
+      <KeyPad buttonPressed={buttonPressed}/>
     </div>
   );
 }
--- src/components/input.js
+++ src/components/input.js
@@ -1,32 +1,37 @@
 import React from 'react';
 
-function KeyPad() {
+function KeyPad(props) {
+    const buttonPressed = e => {props.buttonPressed(e.target.name)}
   return (
     <div>
         <div className="row">
-            <button className="col-3" name="1">1</button>
-            <button className="col-3" name="2">2</button>
-            <button className="col-3" name="3">3</button>
+            <button className="col-3" name="1" onClick={buttonPressed}>1</button>
+            <button className="col-3" name="2" onClick={buttonPressed}>2</button>
+            <button className="col-3" name="3" onClick={buttonPressed}>3</button>
         </div>
         <div className="row">
-            <button className="col-3" name="4">4</button>
-            <button className="col-3" name="5">5</button>
-            <button className="col-3" name="6">6</button>
+            <button className="col-3" name="4" onClick={buttonPressed}>4</button>
+            <button className="col-3" name="5" onClick={buttonPressed}>5</button>
+            <button className="col-3" name="6" onClick={buttonPressed}>6</button>
         </div>
         <div className="row">
-            <button className="col-3" name="7">7</button>
-            <button className="col-3" name="8">8</button>
-            <button className="col-3" name="9">9</button>
+            <button className="col-3" name="7" onClick={buttonPressed}>7</button>
+            <button className="col-3" name="8" onClick={buttonPressed}>8</button>
+            <button className="col-3" name="9" onClick={buttonPressed}>9</button>
         </div>
         <div className="row">
-            <button className="col-3" name="0">0</button>
-            <button className="col-3" name="+">+</button>
-            <button className="col-3" name="-">-</button>
+            <button className="col-3" name="0" onClick={buttonPressed}>0</button>
+            <button className="col-3" name="." onClick={buttonPressed}>.</button>
+            <button className="col-3" name="+" onClick={buttonPressed}>+</button>
         </div>
         <div className="row">
-            <button className="col-3" name="%">%</button >
-            <button className="col-3" name="*">*</button>
-            <button className="col-3" name="CE">CE</button>
+            <button className="col-3" name="-" onClick={buttonPressed}>-</button>
+            <button className="col-3" name="%" onClick={buttonPressed}>%</button>
+            <button className="col-3" name="*" onClick={buttonPressed}>*</button>
+        </div>
+        <div className="row">
+            <button className="col-3" name="=" onClick={buttonPressed}>=</button>
+            <button className="col-3" name="CE" onClick={buttonPressed}>CE</button>
         </div>
     </div>
   );
